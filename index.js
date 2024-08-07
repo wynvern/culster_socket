1bfef1ab6aaee74520a6ee76febed1e042a746fc
wynvern <jeakarllo@gmail.com>
2024-08-07 13:29:34 -0300

    chore: Update http module import to use node:http

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const http = require("http");
+const http = require("node:http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const { decryptToken } = require("./lib/socket");
